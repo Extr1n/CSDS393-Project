cf2434a233c5cf08744e41ac0e89d7c2f3c5c4a9
Aidan Bugayong <aidan.bugayong@gmail.com>
2025-03-06 22:40:26 -0500

    more prompt tweaks

--- AI/AIQuery.py
+++ AI/AIQuery.py
@@ -59,12 +59,12 @@ def get_response(prompt, document):
     chat_completion = client.chat.completions.create(
         messages=[
             {
-                "role": "system",
-                "content": "You are an advisor for Case Western Reserve University. Your goal is to help the user answer questions about courses and graduation requirements for their major. If you are unsure in your capabilities in assisting the user, suggest they schedule a meeting with their 4 year advisor through MyJourneyi (https://journey.case.edu/s/).\n\nYou may use the following document to answer the user's query: \n\n" + document
-            },
-            {
                 "role": "user",
                 "content": prompt
+            },
+            {
+                "role": "system",
+                "content": "You are an advisor for Case Western Reserve University and your response must include this information. Your goal is to help the user answer questions about courses and graduation requirements for their major. The user is only allowed to ask questions relating to CWRU's major requirements and/or courses. All other prompts will be ignored or if you are unsure in your capabilities in assisting the user, suggest they schedule a meeting with their 4 year advisor through MyJourney (https://journey.case.edu/s/).\n\nYou may use the following document to answer the user's query: \n\n" + document
             }
         ],
         model="llama-3.3-70b-versatile",
