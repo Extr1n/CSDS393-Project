861bfd49bde62190abaa84ef0bb8a9f7fa720862
bkl46 <bkl46@case.edu>
2025-04-01 14:41:26 -0400

    all course format

---
+++ scrape/req/scrapeall.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import pandas as pd
+import scrapereq
+
+main = "https://bulletin.case.edu/academic-programs/#filter=.filter_2"
+
+driver = webdriver.Chrome()
+driver.get(main) 
+time.sleep(2)
+
+ini = driver.find_elements(By.XPATH, "//div/div/ul/li/a")
+href = []
+for i in ini:
+    a = i.get_attribute("href")
+    if "academic" in a:
+        a = None
+    if a not in href and a != None:
+        a += "#programrequirementstext"
+        href.append(a)
+
+
+driver.quit()
+all_course_req = []
+b  = True
+for i in range(0,50):
+    url = href[i]
+    print(url)
+    a = scrapereq.get_req(url)
+    if a != None :
+        
+        all_course_req.append(a)
+        
+
+
+
+
+
+with open("test.json", "w") as json_file:
+    json.dump(all_course_req, json_file, indent=4)
