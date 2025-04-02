e12b1550b0cd74a7e56a105dbfe23cf8270f04c2
natlee1122 <nxl353@case.edu>
2025-04-02 00:40:10 -0400

    add more unit tests and logging

--- scrape/exportToDB.py
+++ scrape/exportToDB.py
@@ -6,13 +6,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Load the contents of allcourse.json
-with open('./scrape/parsed_courses.json') as f:
+with open('./scrape/req/test.json') as f:
     data = json.load(f)
 
 # Connect to your MongoDB database
 client = MongoClient(os.getenv('MONGODB_URI'))
 db = client['Documents']
-collection = db['Courses']
+collection = db['Requirements']
 
 # Insert the contents of allcourse.json into the database
 collection.insert_many(data)
