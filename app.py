775e752c39caa5ef254caf5336367fe70aa90f7a
Extr1n <aidan.bugayong@gmail.com>
2025-04-01 21:41:52 -0400

    DB stuff

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # MongoDB connection
-client = MongoClient(os.getenv('DB_key'))
+client = MongoClient(os.getenv('MONGODB_URI'))
 db = client.cluster0
 courses_collection = db["Documents.Courses"]
 user_courses_collection = db.user_courses
